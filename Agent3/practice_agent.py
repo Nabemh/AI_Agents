4d9943534a42310ee2cd3ce8666619ccedec933a
Nabemh <nhanior@gmail.com>
2025-05-29 19:51:39 +0100

    google config

---
+++ Agent3/practice_agent.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-001",
+    temperature=0.7
+    )
+
+prompt = ChatPromptTemplate.from_template(
+    "Explain {concept} in a way a five year old can understand. Use less than 100 words"
+)
+
+final_prompt = prompt.format(concept="Subnetting")
+print(final_prompt)
