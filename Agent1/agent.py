ba57e5bf350a01ed6714fd61fafeed50339c836d
Nabemh <nhanior@gmail.com>
2025-05-31 09:41:48 +0100

    new agent

--- Agent1/agent.py
+++ Agent1/agent.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import Tool, initialize_agent
+from langchain.agents import initialize_agent, AgentType
+from langchain_community.agent_toolkits.load_tools import load_tools
+
 
 load_dotenv()
 
@@ -10,26 +12,15 @@ api_key = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(
     model="gemini-pro",
     temperature=0.7,
-    google_api_key=api_key
-    )
+)
 
-def echo_tool(input : str) -> str:
-    return input
-
-tools = [
-    Tool(
-        name="Echo",
-        func=echo_tool,
-        description="Echoes back the input that has been passed"
-    )
-]
+tools = load_tools(["llm-math", "wikipedia"], llm=llm)
 
 agent = initialize_agent(
-    tools, 
-    llm, 
-    agent="zero-shot-react-description", 
+    tools,
+    llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True
 )
 
-response = agent.run("Hello, what can you do?")
-print(response)
+agent.run("What's 25% of 80? Then summarize the Wikipedia page for giraffes.")
