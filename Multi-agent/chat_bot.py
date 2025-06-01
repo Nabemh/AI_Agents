aa0dde436b97f74cb9f5fbc7df3affdbe37e50b0
Nabemh <nhanior@gmail.com>
2025-06-01 13:48:31 +0100

    customer support bot

--- Multi-agent/chat_bot.py
+++ Multi-agent/chat_bot.py
@@ -6,15 +6,19 @@ import pandas as pd
 from langchain.agents import initialize_agent, AgentType, Tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.memory import ConversationBufferMemory
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 load_dotenv()
 
 api_key = os.getenv("GOOGLE_API_KEY")
+tavily_api_key = os.getenv("TAVILY_API_KEY")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash-001",
     temperature=0.7,
 )
+
+
 
 dataset = load_dataset("MakTek/Customer_support_faqs_dataset", split="train")
 df = dataset.to_pandas()
@@ -35,8 +39,21 @@ support_tools = [
     )
 ]
 
+search_tool = TavilySearchResults(api_key=tavily_api_key, max_results=3)
+
+research_tool = Tool(
+    name="Research tool",
+    func=lambda query: search_tool.run(f"Search online for {query}"),
+    description="Get generic information about this and return in Structured bullet points."
+                """- Under 'How you sign in to Google,' tap Password. You might need to sign in.
+                   - Enter your new password, then tap Change Password.
+                        - Tip: When you enter your password on mobile, the first letter isn't case sensitive."""
+)
+
+all_tools = support_tools + [research_tool]
+
 support_agent = initialize_agent(
-    tools=support_tools,
+    tools=all_tools,
     llm=llm,
     agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
     memory=ConversationBufferMemory(memory_key="chat_history"),
