60b23582e8fb43cfe35aa037754e6c91fe42a416
Nabemh <nhanior@gmail.com>
2025-06-04 10:45:55 +0100

    docs

---
+++ Doc_AI/vector_store.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate
+
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-001",
+    temperature=0.7,
+)
+
