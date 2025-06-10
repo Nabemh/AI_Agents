fe88811b78e0f94c52794f1cf725b92b50bae045
Nabemh <nhanior@gmail.com>
2025-06-10 13:18:39 +0100

    vector stores

--- Multi_Rag/Multi_rag.py
+++ Multi_Rag/Multi_rag.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain.document_loaders import PyPDFLoader, WebBaseLoader, CSVLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import ChatPromptTemplate
+from langchain.vectorstores import FAISS
 
 load_dotenv()
 
@@ -15,8 +16,8 @@ llm = ChatGoogleGenerativeAI(
 )
 
 pdf_loader = PyPDFLoader("policy.pdf")
-web_loader = WebBaseLoader("")
-csv_loader = CSVLoader("")
+web_loader = WebBaseLoader("https://www.typetone.ai/blog/faq-examples-100-faqs-for-your-landing-page")
+csv_loader = CSVLoader("LIST OF Physican and Virtual attendance.xlsx - Physical Attandance.csv")
 
 pdf_pages = pdf_loader.load()
 web_pages = web_loader.load()
@@ -26,7 +27,7 @@ all_docs = pdf_pages + web_pages + csv_doc
 
 
 
-splitter = RecursiveCharacterTextSplitter( 
+splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000,
     chunk_overlap=200,
     separators=["\n\n", "\n", ". ", " "]
@@ -34,21 +35,28 @@ splitter = RecursiveCharacterTextSplitter(
 
 chunks = splitter.split_documents(all_docs)
 
-prompt = ChatPromptTemplate.from_template("""  
-Answer the question using ONLY these sources. Cite each source like [1], [2].  
+embeddings = ChatGoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vectorstore = FAISS.from_documents(chunks, embeddings)
+vectorstore.save_local("multidoc_index")
 
-Sources:  
-{context}  
+def retrieve ():
+    return
 
-Question: {question}  
-""")  
+prompt = ChatPromptTemplate.from_template("""
+Answer the question using ONLY these sources. Cite each source like [1], [2].
 
-rag_chain = prompt | llm  
+Sources:
+{context}
 
-def ask(query, sources=None):  
-    docs = retrieve(query, sources)  
-    context = "\n\n".join(  
-        f"[{i+1}] {d.page_content} (Source: {d.metadata['source']})"  
-        for i, d in enumerate(docs)  
-    )  
-    return rag_chain.invoke({"context": context, "question": query})  
+Question: {question}
+""")
+
+rag_chain = prompt | llm
+
+def ask(query, sources=None):
+    docs = retrieve(query, sources)
+    context = "\n\n".join(
+        f"[{i+1}] {d.page_content} (Source: {d.metadata['source']})"
+        for i, d in enumerate(docs)
+    )
+    return rag_chain.invoke({"context": context, "question": query})
