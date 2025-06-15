da58cf2e1643471d2c56849b0d67296b564894fe
Nabemh <nhanior@gmail.com>
2025-06-15 21:05:30 +0100

    lol

--- Work/Report.py
+++ Work/Report.py
@@ -15,12 +15,3 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.7
 )
 
-pdf_loader = PyPDFLoader("policy.pdf")
-web_loader = WebBaseLoader("https://www.typetone.ai/blog/faq-examples-100-faqs-for-your-landing-page")
-csv_loader = CSVLoader("LIST OF Physican and Virtual attendance.xlsx - Physical Attandance.csv")
-
-pdf_pages = pdf_loader.load()
-web_pages = web_loader.load()
-csv_doc = csv_loader.load()
-
-all_docs = pdf_pages + web_pages + csv_doc
